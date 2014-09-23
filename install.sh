72464bd4ba1778f9dd023feefa12713a063437c0
Ketan Padegaonkar <KetanPadegaonkar@gmail.com>
2014-09-23 13:45:29 +0530

    Apparently, PATH cannot be inherited for security reasons

--- install.sh
+++ install.sh
@@ -3,14 +3,16 @@
 set -e
 
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}" )" > /dev/null && pwd)"
-
 cd $SCRIPT_DIR
+
+export NOKOGIRI_USE_SYSTEM_LIBRARIES=1
+export PATH="/opt/local/ruby/2.0.0-p353/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin"
 
 # change ownership to nobody to not screw up things (even accidentally)
 chown nobody.nobody -R $SCRIPT_DIR
 
 # perform the bundle install
-sudo -E -u nobody NOKOGIRI_USE_SYSTEM_LIBRARIES=1 PATH="/opt/local/ruby/2.0.0-p353/bin:$PATH" $(which bundle) install --local --standalone --clean
+sudo -t -E -u nobody -- env PATH=$PATH NOKOGIRI_USE_SYSTEM_LIBRARIES=1 $(which bundle) install --local --standalone --clean
 
 # change ownership back to root
 chown root.root -R $SCRIPT_DIR
