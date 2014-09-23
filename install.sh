a38d5ac9c38a5961c7a36dbe36d545e3947165e6
Ketan Padegaonkar <KetanPadegaonkar@gmail.com>
2014-09-23 13:14:54 +0530

    Fix typo

--- install.sh
+++ install.sh
@@ -7,10 +7,10 @@ SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}" )" > /dev/null && pwd)"
 cd $SCRIPT_DIR
 
 # change ownership to nobody to not screw up things (even accidentally)
-chown nobody.nobody -R /opt/local/snap-deploy
+chown nobody.nobody -R $SCRIPT_DIR
 
 # perform the bundle install
-sudo NOKOGIRI_USE_SYSTEM_LIBRARIES=1 PATH="/opt/local/ruby/2.0.0-p353/bin:$PATH" -t -u nobody $(which bundle) install --local --standalone --clean"
+sudo -u nobody NOKOGIRI_USE_SYSTEM_LIBRARIES=1 PATH="/opt/local/ruby/2.0.0-p353/bin:$PATH" "$(which bundle) install --local --standalone --clean"
 
 # change ownership back to root
-chown root.root -R /opt/local/snap-deploy
+chown root.root -R $SCRIPT_DIR
