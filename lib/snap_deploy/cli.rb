0e7a687049e20713cadbe6f4ecf703f390d68020
Ketan Padegaonkar <KetanPadegaonkar@gmail.com>
2014-11-26 18:21:59 +0530

    Add a version flag

--- lib/snap_deploy/cli.rb
+++ lib/snap_deploy/cli.rb
@@ -7,6 +7,10 @@ module SnapDeploy
     module DefaultOptions
       def self.included(receiver)
         receiver.send :option, '--verbose', :flag, 'increase verbosity'
+        receiver.send :option, '--version', :flag, 'display the version' do
+          puts "Snap Deploy v#{SnapDeploy::VERSION}"
+          exit(0)
+        end
       end
     end
 
